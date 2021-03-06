ce3bd35be4a61c3cc683138532325cafa10b77f0
Clemens-Dautermann <clemens.dautermann@t-online.de>
2021-03-06 23:26:40 +0100

    included support for optional and make dependencies

--- searcher.js
+++ searcher.js
@@ -1,11 +1,14 @@
-function search(pPackageName) {
+async function search(pPackageName) {
 
     //remove version declarations because the api cannot handle that
     let packageName = pPackageName.replace(/(>|>=|=).*/, '');
 
+    //TODO: Once the bug is fixed, this should work without CORS Proxy
     const proxy = 'https://archviz-proxy.herokuapp.com/'
 
+    //make the fetch request
     return fetch(
+        //set the appropriate settings
         proxy + "https://archlinux.org/packages/search/json/?name=" + packageName, {
             method: 'GET',
             mode: 'cors',
@@ -14,18 +17,49 @@ function search(pPackageName) {
             }
         }
     )
+        //make the response into json first
         .then(response => response.json())
         .then(json => {
-            return new Set(json['results'][0]['depends'].values());
+            //set the error message to '' (because obviously something was found)
+            document.getElementById('ErrorMessageBox').innerText = '';
+            //return the dependencys
+            const allDeps = new Set(json['results'][0]['depends'].values());
+
+
+            //include optional dependencies if wanted
+            if (document.getElementById('includeOptionalDepsCheckbox').checked) {
+                //iterate over all optional dependencies
+                for (const optionalDepWithComment of json['results'][0]['optdepends'].values()) {
+                    //add them to all dependencies
+                    allDeps.add(optionalDepWithComment.replace(/:\s.*/, ''));
+                }
+            }
+
+            //include make dependencies if wanted
+            if (document.getElementById('includeMakeDepsCheckbox').checked) {
+                //add the make dependencys
+                for (const makedep of json['results'][0]['optdepends'].values()) {
+                    allDeps.add(makedep);
+                }
+            }
+
+            return allDeps;
         })
         .catch(error => {
+                //log the error
                 console.error(`cannot resolve package ${packageName}`);
+                //if this was the root package search, set the error message box
+                if (packageName === document.getElementById('packageInput').value) {
+                    document.getElementById('ErrorMessageBox').innerText =
+                        `cannot resolve package "${packageName}"`;
+                }
+                //return an empty set so that iterating in the recursion will not break
                 return new Set();
             }
         );
 }
 
-function addSubteree(rootNode, graph, cache) {
+async function addSubteree(rootNode, graph, cache) {
 
     //retrieve all dependencies of a package using the serch function
     search(rootNode).then((dependencies) => {
