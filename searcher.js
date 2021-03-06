c3baa0e7717548f265b3d52395076ed160bb829a
Clemens-Dautermann <clemens.dautermann@t-online.de>
2021-03-06 21:44:40 +0100

    dependency tree drawing works now

--- searcher.js
+++ searcher.js
@@ -1,8 +1,8 @@
-async function search(packageName) {
+function search(packageName) {
 
     const proxy = 'https://archviz-proxy.herokuapp.com/'
 
-    return await fetch(
+    return fetch(
         proxy + "https://archlinux.org/packages/search/json/?name=" + packageName, {
             method: 'GET',
             mode: 'cors',
@@ -13,9 +13,38 @@ async function search(packageName) {
     )
         .then(response => response.json())
         .then(json => {
-            return json['results'][0]['depends'];
+            return new Set(json['results'][0]['depends'].values());
         })
-        .catch(error =>
-            console.error(error)
+        .catch(error => {
+                console.error(`cannot resolve package ${packageName}`);
+                return new Set();
+            }
         );
+}
+
+function addSubteree(rootNode, graph, cache) {
+
+    //retrieve all dependencies of a package using the serch function
+    search(rootNode).then((dependencies) => {
+
+        //iterate over all those dependencies
+        for (const dependency of dependencies) {
+
+            //check if the dependency is cached (if the subtree has already been drawn for it
+            if (!cache.has(dependency)) {
+                //add a link from the rootnode to that dependency
+                graph.addLink(rootNode, dependency);
+
+                //initiate recursive call to build subtree for that dependency
+                addSubteree(
+                    dependency,
+                    graph,
+                    cache
+                );
+
+                //add the dependency to the cache so that in the next loop iteration it will not be handled again
+                cache.add(dependency);
+            }
+        }
+    });
 }
