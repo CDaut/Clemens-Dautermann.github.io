285e2fa5d007770714207b044d619fa3f723721e
Clemens-Dautermann <clemens.dautermann@t-online.de>
2021-03-07 17:56:49 +0100

    Implemented color based on the repository

--- searcher.js
+++ searcher.js
@@ -1,7 +1,7 @@
-async function search(pPackageName) {
+async function getNodeJSON(pPackageName) {
 
-    //remove version declarations because the api cannot handle that
-    let packageName = pPackageName.replace(/(>|>=|=).*/, '');
+    //remove version declarations and comments because the api cannot handle that
+    let packageName = pPackageName.replace(/(>|>=|=).*/, '').replace(/:\s.*/, '');
 
     //TODO: Once the bug is fixed, this should work without CORS Proxy
     const proxy = 'https://archviz-proxy.herokuapp.com/'
@@ -20,30 +20,10 @@ async function search(pPackageName) {
         //make the response into json first
         .then(response => response.json())
         .then(json => {
-            //set the error message to '' (because obviously something was found)
-            document.getElementById('ErrorMessageBox').innerText = '';
-            //return the dependencys
-            const allDeps = new Set(json['results'][0]['depends'].values());
+            //try to get the dependencies trigger an error if they are absent
+            if (typeof json['results'][0]['depends'].values() === "undefined") throw Error;
 
-
-            //include optional dependencies if wanted
-            if (document.getElementById('includeOptionalDepsCheckbox').checked) {
-                //iterate over all optional dependencies
-                for (const optionalDepWithComment of json['results'][0]['optdepends'].values()) {
-                    //add them to all dependencies
-                    allDeps.add(optionalDepWithComment.replace(/:\s.*/, ''));
-                }
-            }
-
-            //include make dependencies if wanted
-            if (document.getElementById('includeMakeDepsCheckbox').checked) {
-                //add the make dependencys
-                for (const makedep of json['results'][0]['optdepends'].values()) {
-                    allDeps.add(makedep);
-                }
-            }
-
-            return allDeps;
+            return json['results'][0];
         })
         .catch(error => {
                 //log the error
@@ -59,10 +39,45 @@ async function search(pPackageName) {
         );
 }
 
+function getDepsFromJSON(json) {
+    //set the error message to '' (because obviously something was found)
+    document.getElementById('ErrorMessageBox').innerText = '';
+
+    //check if the json is valid
+    if (typeof json['depends'] === 'undefined') return new Set();
+
+    //return the dependencys
+    const allDeps = new Set(json['depends'].values());
+
+
+    //include optional dependencies if wanted
+    if (document.getElementById('includeOptionalDepsCheckbox').checked) {
+        //iterate over all optional dependencies
+        for (const optionalDepWithComment of json['optdepends'].values()) {
+            //add them to all dependencies
+            allDeps.add(optionalDepWithComment.replace(/:\s.*/, ''));
+        }
+    }
+
+    //include make dependencies if wanted
+    if (document.getElementById('includeMakeDepsCheckbox').checked) {
+        //add the make dependencys
+        for (const makedep of json['optdepends'].values()) {
+            allDeps.add(makedep);
+        }
+    }
+    return allDeps;
+}
+
 async function addSubteree(rootNode, graph, cache) {
 
-    //retrieve all dependencies of a package using the serch function
-    search(rootNode).then((dependencies) => {
+    //retrieve node JSON
+    getNodeJSON(rootNode).then((nodeJSON) => {
+
+        const dependencies = getDepsFromJSON(nodeJSON);
+        //add the node to the graph while replacing the unnecessary additional text
+        graph.addNode(rootNode.replace(/:\s.*/, ''),
+            JSON.parse(`{"repository": "${nodeJSON['repo']}"}`));
 
         //iterate over all those dependencies
         for (const dependency of dependencies) {
